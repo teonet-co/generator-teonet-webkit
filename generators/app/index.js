69184e88112e816bfedf897274357a3aeed5759e
Kirill Scherba <kirill@scherba.ru>
2016-08-03 21:41:58 +0300

    name of generator in welcome message updated

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,7 +7,7 @@ module.exports = yeoman.Base.extend({
   prompting: function () {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the extraordinary ' + chalk.red('TEONET-webkit-APPLICATION') + ' generator!'
+      'Welcome to the extraordinary ' + chalk.red('TEONET-WEBKIT-APPLICATION') + ' generator!'
     ));
 
     var prompts = [{
